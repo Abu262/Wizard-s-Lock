4d886fc9cf364e714bbfe442b0d5b714f34af17a
gabeeck <eck.gabe@gmail.com>
2019-02-06 15:00:20 -0800

    Added canvas for UI and a timer
    timer counts down in Minutes:Seconds

---
+++ Wizard's Lock V2/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    TextMeshProUGUI timerText;
+    float timeLeft = 1200f;
+    float timeMin = 0f;
+    float timeSec = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = this.GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeMin = timeLeft / 60;
+        timeSec = timeLeft % 60;
+        timerText.text = string.Format("{0}:{1}", (int) timeMin, (int) timeSec);
+    }
+}
