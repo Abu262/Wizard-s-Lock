86c8af4977625b4ee3fd1161b292144a8d21e6c5
gabeeck <eck.gabe@gmail.com>
2019-02-13 14:10:04 -0800

    Item and chess pickup/movement scripts

--- Wizard's Lock V2/Assets/Scripts/Chess.cs
+++ Wizard's Lock V2/Assets/Scripts/Chess.cs
@@ -4,14 +4,68 @@ using UnityEngine;
 
 public class Chess : MonoBehaviour
 {
+    [SerializeField] protected bool isPicked = false;
+    [SerializeField] GameObject selectedObject = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && selectedObject == null)
+            selectedObject = ClickSelect();
+
+        if (Input.GetMouseButtonDown(0) && selectedObject != null)
+            DropObject();
+
+        MoveObject();
+
+        CheckBoard();
+
+    }
+
+    GameObject ClickSelect()
+    {
+        //Converting Mouse Pos to 2D (vector2) World Pos
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            Debug.Log(hit.transform.name);
+            return hit.transform.gameObject;
+        }
+        else return null;
+    }
+
+    void MoveObject()
+    {
+        if (selectedObject != null && !selectedObject.CompareTag("ChessBoard") && !selectedObject.CompareTag("White"))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedObject.transform.position = mousePos;
+        }
+    }
+
+    void DropObject()
+    {
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            selectedObject.transform.position = hit.transform.position;
+            selectedObject = null;
+        }
+
+        
+    }
+
+    void CheckBoard()
     {
         
     }
--- Wizard's Lock V2/Assets/Scripts/GameManager.cs
+++ Wizard's Lock V2/Assets/Scripts/GameManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    [SerializeField] protected bool isPicked = false;
+    [SerializeField] GameObject selectedObject = null;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,27 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        selectedObject = ClickSelect();
+
+        if (selectedObject != null)
+        {
+
+        }
+
+
+    }
+
+    GameObject ClickSelect()
+    {
+        //Converting Mouse Pos to 2D (vector2) World Pos
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            Debug.Log(hit.transform.name);
+            return hit.transform.gameObject;
+        }
+        else return null;
     }
 }
