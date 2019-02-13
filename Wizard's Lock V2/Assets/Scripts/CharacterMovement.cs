6e0c3d12b896752becc3c6b09f2bf565652eb3b8
asgaria1 <ariannaasgari@gmail.com>
2019-02-13 14:03:22 -0800

    Added character movement

---
+++ Wizard's Lock V2/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private bool moving;
+    private Vector3 movedTo;
+    public float speed;
+    //public GameObject chara;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the player moving left right, up or down
+        if (Input.GetMouseButtonDown(0)) {
+            movedTo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movedTo.z = transform.position.z;
+            if( !moving ){
+                moving = true;
+            }
+            //Instantiate( chara, movedTo, Quaternion.identity );
+        }
+
+        if ( moving ){
+            transform.position = Vector3.MoveTowards(transform.position, movedTo, speed * Time.deltaTime);
+        }
+        //float horizontal = Input.GetButtonDown("Mouse X");
+        //float vertical = Input.GetAxis("Vertical");
+
+        //storing movement in a 2d vector
+        //Vector2 moving = new Vector2(horizontal, vertical);
+
+        //give the rigidbody movement * how fast we want them to move
+        //rigidBody.AddForce(moving * speed);
+    }
+}
