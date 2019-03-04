d083185e2ca6f1d5b8e2a871825ba70dfd0fb633
Jaren Gerdes <gerdesjaren@gmail.com>
2019-03-04 15:10:36 -0800

    Updated potions and ItemObject script

--- Wizard's Lock V2/Assets/Scripts/ItemObject.cs
+++ Wizard's Lock V2/Assets/Scripts/ItemObject.cs
@@ -6,14 +6,20 @@ public class ItemObject : Inventory
 {
     void Awake()
     {
-
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Item Added By Click");
-            AddItem(2);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log("Item Added By Click");
+                AddItem(2);
+            }
         }
     }
 }
