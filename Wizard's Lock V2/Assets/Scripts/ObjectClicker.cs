6678dd483999e70a296dd892ba6a9dc250194d37
dsopi <diellza.kosova1@gmail.com>
2019-03-04 22:43:53 -0800

    Added in menu buttons to each main scene and interactable item

--- Wizard's Lock V2/Assets/Scripts/ObjectClicker.cs
+++ Wizard's Lock V2/Assets/Scripts/ObjectClicker.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ObjectClicker : MonoBehaviour
 {
-    public Button addtoInventory, interactWithItem, lookAtItem;
+    public GameObject addtoInventory, interactWithItem, lookAtItem;
     public Vector3 pos;
     public Collider2D hitCollider;
     public Vector3 colliderPosition;
@@ -27,35 +27,28 @@ public class ObjectClicker : MonoBehaviour
 
             if (hitCollider != null)
             {
-                colliderPosition = hitCollider.transform.position; 
+                colliderPosition = hitCollider.transform.position;
                 addtoInventory.transform.position = new Vector3(pos.x, (pos.y + 100),pos.z);
                 interactWithItem.transform.position = new Vector3(pos.x, (pos.y + 70), pos.z);
                 lookAtItem.transform.position = new Vector3(pos.x, (pos.y + 40), pos.z);
                 SetButtons(true);
-
-                lookAtItem.onClick.AddListener(ItemDescription);
             }
+
             else
             {
                 SetButtons(false);
             }
+
         }
+
     }
 
     void SetButtons(bool itemClicked)
     {
-        addtoInventory.gameObject.SetActive(itemClicked);
-        interactWithItem.gameObject.SetActive(itemClicked);
-        lookAtItem.gameObject.SetActive(itemClicked);
+        addtoInventory.SetActive(itemClicked);
+        interactWithItem.SetActive(itemClicked);
+        lookAtItem.SetActive(itemClicked);
     }
-
-    void ItemDescription()
-    {
-        string tooltip = "meow";
-        Debug.Log("meow");
-
-        GUI.Box(new Rect(0, 0, Screen.height, Screen.width), tooltip);
-
-    }
+    
 
 }
